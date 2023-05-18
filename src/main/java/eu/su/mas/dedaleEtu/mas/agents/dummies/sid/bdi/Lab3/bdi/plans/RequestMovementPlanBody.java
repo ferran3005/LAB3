bb7f379824d2b69cc125900d5bcfd0062a6aebb7
Baqu-J <jaume.baquero@glovoapp.com>
2023-05-18 18:30:20 +0200

    Plan structure

---
+++ src/main/java/eu/su/mas/dedaleEtu/mas/agents/dummies/sid/bdi/Lab3/bdi/plans/RequestMovementPlanBody.java
@@ -0,0 +1,10 @@
+package eu.su.mas.dedaleEtu.mas.agents.dummies.sid.bdi.Lab3.bdi.plans;
+
+import bdi4jade.plan.planbody.BeliefGoalPlanBody;
+
+public class RequestMovementPlanBody extends BeliefGoalPlanBody {
+    @Override
+    protected void execute() {
+        // TODO
+    }
+}
